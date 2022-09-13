29871cd87dfd40585f94a2d90e7f1be9adf53cb9
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-09-13 20:32:48 -0300

    Correções primeira tela do jogo

---
+++ app/src/main/java/com/example/campominado/TelaJogo.java
@@ -0,0 +1,14 @@
+package com.example.campominado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaJogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_jogo);
+    }
+}
