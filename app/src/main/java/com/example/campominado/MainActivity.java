061256a2d8af386bd231789a0034a87040947576
Tércio Tales Barnabé <terciotalesb@gmail.com>
2022-09-13 20:50:49 -0300

    Adiciona ícone no app

--- app/src/main/java/com/example/campominado/MainActivity.java
+++ app/src/main/java/com/example/campominado/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.campominado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button play;
+    Button quit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
